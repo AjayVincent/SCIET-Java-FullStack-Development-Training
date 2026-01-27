4f75954d683d0f620f797076914e6ae37e7d13d9
Poojitha Bonagiri <ajayvincentbonagiri@gmail.com>
2026-01-27 18:18:06 +0530

    Created

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] array) {
+        System.out.println("Hello !");
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+    }
+}
